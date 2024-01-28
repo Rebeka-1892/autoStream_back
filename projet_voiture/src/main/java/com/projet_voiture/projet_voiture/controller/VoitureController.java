8155a9775f06fbd8683725721131e93e9a4ee3c2
Rebeka-1892 <etu1892@gmail.com>
2024-01-28 21:56:03 +0300

    photo voiture

--- projet_voiture/src/main/java/com/projet_voiture/projet_voiture/controller/VoitureController.java
+++ projet_voiture/src/main/java/com/projet_voiture/projet_voiture/controller/VoitureController.java
@@ -2,10 +2,13 @@ package com.projet_voiture.projet_voiture.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
- 
 
+import com.projet_voiture.projet_voiture.modele.Marque;
 import com.projet_voiture.projet_voiture.modele.Voiture;
 import com.projet_voiture.projet_voiture.service.VoitureService;
+import com.projet_voiture.projet_voiture.util.ImageService;
+import com.projet_voiture.projet_voiture.util.Photo;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,7 +22,13 @@ public class VoitureController {
      
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Voiture insert(@RequestBody Voiture Voiture) {
+    public Voiture insert(@RequestBody Voiture Voiture) throws Exception {
+        for (int i = 0; i < Voiture.getPhotos().length; i++) {            
+            Photo photo = new Photo(Voiture.getPhotos()[i], "photovoiture.png");
+            ImageService imageService = new ImageService();
+            String url = imageService.upload(photo);
+            Voiture.getPhotos()[i] = url;
+        }
         return service.insert(Voiture);
     }
 
